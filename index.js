33711952dd0de0efbe8f52d4eaf69df36672c282
Rich Harris <richard.a.harris@gmail.com>
2014-09-14 23:12:42 -0400

    use save and restore ANSI codes to avoid loader being printed on sequential lines

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = function ( options ) {
 
-	var loader, message, frames, interval, timeout, i = 0, tick, update, stream, tty, CARRIAGE_RETURN;
+	var loader, message, frames, interval, timeout, i = 0, tick, update;
 
 	options = options || {};
 
@@ -17,15 +17,9 @@ module.exports = function ( options ) {
 	];
 	interval = options.interval || 100;
 
-	stream = process.stderr;
-	tty = stream.isTTY;
-
-	// thanks, https://github.com/isaacs/char-spinner/blob/master/spin.js
-	CARRIAGE_RETURN = stream.isTTY ? '\u001b[0G' : '\u000d';
-
 	tick = function () {
 		var frame = frames[ i++ % frames.length ];
-		stream.write( frame + ' ' + message + CARRIAGE_RETURN );
+		process.stderr.write( '\u001b[s' + frame + ' ' + message + '\u001b[u' );
 	};
 
 	update = function () {
